6b8415d1affeca2401ab7cb99973a9261cf91c8f
Gautam Gupta <123723190+gautam1025@users.noreply.github.com>
2025-07-12 18:40:17 +0530

    Update (created components)

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,20 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 const App = () => {
-
-  const [username, setUsername] = useState('')
-
-  const submitHandler = (e)=>{
-    e.preventDefault()
-    console.log(username)
-    setUsername('')
-  }
-
   return (
     <div>
-      <form onSubmit={(e)=>{
-        submitHandler(e)
-      }}>
-        <input 
-        value={username}
-        onChange={(e)=>{
-          setUsername(e.target.value)
-        }
-
-        }
-        className='bg-white text-xl rounded px-3 py-4 m-5' 
-        type='text'
-        placeholder='Enter you name'
-        ></input>
-        <button className='text-xl px-3 py-4 m-5 bg-yellow-500 rounded'>Submit</button>
-      </form>
-    </div>
+      <>
+        <nav className='bg-blue-900 flex px-10 py-5 items-center justify-between'>
+          <h2 className='text-2xl'>Gautam Gupta</h2>
+          <div className='flex gap-10 items-center'>
+            <h4 className='text-xl'>Your Account</h4>
+            <h4 className='text-xl'>Services</h4>
+            <h4 className='text-xl'>Contact</h4>
+            <h4 className='text-xl'>About</h4>
+          </div>
+        </nav>
+      </>
+    </div> 
   )
 }
 
